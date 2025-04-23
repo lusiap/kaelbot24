9a44af120b9c9c538e60598e452c0d6e81589001
lusiap <roykiyisianjay89@gmail.com>
2025-04-23 19:51:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ upstream_repo = getenv(
 )
 upstream_branch = getenv("upstream_branch", "final")
 git_token = getenv("git_token", None)
-log_channel = getenv("log_channel", None)
+log_channel = getenv("log_channel", "-1002631843717")
 genius_api = getenv(
     "genius_api",
     "zhtfIphjnawHBcLFkIi-zE7tp8B9kJqY3xGnz_BlzQM9nhJJrD7csS1upSxUE0OMmiP3c7lgabJcRaB0hwViow",
